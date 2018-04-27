8202f5313c79f823b9c6626f8ed1e7844fb3ffdf
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-04-27 12:50:07 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
+const allUsers = ['Myszka Mickey', 'Kaczor Donald', 'Pies Pluto', 'Gooffy', 'Chip', 'Dale'];
+
+class App extends React.Component {
+  constructor() {
+    super();    
+    
+    this.state = {
+      filteredUsers: allUsers
+    };
+  }
+
+  filterUsers(e) {
+    const text = e.currentTarget.value;
+    const filteredUsers = this.getFilteredUsersForText(text)
+    this.setState({
+      filteredUsers
+    })
+  }
+  
+  getFilteredUsersForText(text) {
+    return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
+  }
+  
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input onInput={this.filterUsers.bind(this)} />
+        <UsersList users={this.state.filteredUsers} />
       </div>
     );
   }
-}
+};
+
+const UsersList = ({ users }) => {
+  if (users.length > 0) {
+    return (
+      <ul>
+        {users.map(user => <li key={user}>{user}</li>)}
+      </ul>
+    );
+  }
+
+  return (
+    <p>No results!</p>
+  );
+};
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
 
 export default App;
